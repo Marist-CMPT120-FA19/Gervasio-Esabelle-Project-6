edf5121d4d3fd5cc14452c5e0de670376518e710
esag13 <54591108+esag13@users.noreply.github.com>
2019-10-14 18:21:55 -0400

    Update Gervasio-Esabelle-Project-6.py

--- Gervasio-Esabelle-Project-6.py
+++ Gervasio-Esabelle-Project-6.py
@@ -10,7 +10,7 @@ win= GraphWin("Traffic light" , 500, 500)
 
 
 #function that draws the box of the light
-def draw_light_body(x, y, length, width):
+def draw_body(x, y, length, width):
     body = Rectangle(Point(x,y), Point(width, length))
     body.setOutline("black")
     body.setFill("white")
@@ -23,13 +23,10 @@ def draw_lamp(color, x, y, radius):
     lamp.setFill(color)
     lamp.draw(win)
 
-#function that draws the whole traffic light
+#function that draws the traffic light
 def draw_traffic_light(x, y):
-    draw_light_body(20, 20, 170, 80)
+    draw_body(20, 20, 170, 80)
     draw_lamp("red", 50,45, 20)
     draw_lamp("yellow", 50,95, 20)
     draw_lamp("green", 50,145, 20)
-
-
-#calling the function at a specific starting point
 draw_traffic_light(20,20)
