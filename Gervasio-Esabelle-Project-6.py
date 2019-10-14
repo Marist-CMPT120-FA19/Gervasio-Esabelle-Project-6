54bdf2effd59a600ece8f9e1731c905be2ed4985
esag13 <54591108+esag13@users.noreply.github.com>
2019-10-13 22:40:23 -0400

    Add files via upload

---
+++ Gervasio-Esabelle-Project-6.py
@@ -0,0 +1,35 @@
+#Esabelle Gervasio
+#Project 6
+#Traffic Light with functions
+
+from graphics import *
+
+
+#setting the window size and naming the window
+win= GraphWin("Traffic light" , 500, 500)
+
+
+#function that draws the box of the light
+def draw_light_body(x, y, length, width):
+    body = Rectangle(Point(x,y), Point(width, length))
+    body.setOutline("black")
+    body.setFill("white")
+    body.draw(win)
+
+#function that draws the lamp circles
+def draw_lamp(color, x, y, radius):
+    lamp = Circle(Point(x, y), radius)
+    lamp.setOutline("black")
+    lamp.setFill(color)
+    lamp.draw(win)
+
+#function that draws the whole traffic light
+def draw_traffic_light(x, y):
+    draw_light_body(20, 20, 170, 80)
+    draw_lamp("red", 50,45, 20)
+    draw_lamp("yellow", 50,95, 20)
+    draw_lamp("green", 50,145, 20)
+
+
+#calling the function at a specific starting point
+draw_traffic_light(20,20)
